6ad9a2611b39faaea52c8a921e62acced9d529ec
Guilherme Murata <123330836+GuilhermeMurata@users.noreply.github.com>
2024-08-15 20:07:50 -0300

    add casting

---
+++ src/funcoes.ts
@@ -0,0 +1,10 @@
+function calcularArea(base: number, altura: number): number {
+    return base * altura;
+}
+
+const calcularArea2 = (base: number, altura: number): number => base * altura
+
+function somar(...numeros: number[]): void {
+    // numeros.reduce()
+    console.log(numeros);
+}
