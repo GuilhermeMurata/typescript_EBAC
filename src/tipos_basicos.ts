d6a8d02765a8b8302b41e8bfed4b92da1f8bd5a4
Guilherme Murata <123330836+GuilhermeMurata@users.noreply.github.com>
2024-08-15 19:48:21 -0300

    Add typescript - tipos

---
+++ src/tipos_basicos.ts
@@ -0,0 +1,25 @@
+let estaChovendo: boolean = false
+
+estaChovendo = true
+
+let idade: number = 27
+
+let altura: number = 1.75
+
+const nacionalidade: string = 'brasileira'
+
+const colegas: string[] = ['lucas', 'fernanda']
+
+const tecnologias: Array<string> = ['html', 'css']
+
+const notas: ReadonlyArray<number> = [2, 8]
+
+const lista: [nome: string, estaEstudando: boolean, idade: number] = ['Murata', true, 47]
+
+let idadeDaAna: number | string = '25' //or 25
+
+let dadosDaApi: any;
+dadosDaApi = 10
+dadosDaApi = true
+dadosDaApi = [1, 2, 3]
+dadosDaApi = 'string'
