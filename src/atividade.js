07c7540dcfc43b01704bc3663810ad49cc1a3dec
Guilherme Murata <123330836+GuilhermeMurata@users.noreply.github.com>
2024-08-29 18:22:51 -0300

    Atividade EBAC

---
+++ src/atividade.js
@@ -0,0 +1,9 @@
+"use strict";
+function math(Pnum, Snum) {
+    return Pnum * Snum;
+}
+console.log(math(5, 3));
+function dizOla(name) {
+    return `Ola ` + name;
+}
+console.log(dizOla('Batista'));
---
+++ src/atividade.ts
@@ -0,0 +1,12 @@
+function math(Pnum: number, Snum: number) {
+    return Pnum * Snum;
+}
+
+console.log(math(5, 3));
+
+function dizOla(name: string) {
+    return `Ola ` + name;
+}
+
+console.log(dizOla('Batista'));
+
